be2624fd965794eaaa79770d870cc25af5cbab4a
Dongbin Suh <dongbinsuh@gmail.com>
2020-03-02 22:31:46 -0500

    navBar done

--- kiosk/src/components/navBar.js
+++ kiosk/src/components/navBar.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import { NAVLOGO } from '../assets';
 
+/*
+    This is navigation bar component to be included
+    in every page.
+
+    How to use this component
+        1. onGuides    :  button event when pushed onGuides navlink
+        2. onTowns     :  button event when pushed onTowns navlink
+        3. onEvents    :  button event when pushed onEvents navlink
+        4. onDirectory :  button event when pushed onDirectory navlink
+        5. onAbout     :  button event when pushed onAbout navlink
+
+    usage example:
+        <NavBar
+            onGuides={()=>someFunc1()}
+            onTowns={()=>someFunc2()}
+            onEvents={()=>someFunc3()}
+            onDirectory={()=>someFunc4()}
+            onAbout={()=>someFunc5()}
+        />
+
+    font is not downloaded yet.
+    Will be updated when getting info from Garret and Alex
+*/
+
 const NavBar = (props)=>{
     const {
-
+        onGuides,         
+        onTowns,
+        onEvents,
+        onDirectory,
+        onAbout
     } = props;
 
     return (
@@ -19,27 +47,27 @@ const NavBar = (props)=>{
                 <div className="floatR">
                     <ul className="nav">
                         <li className="nav-item">
-                            <a onClick={()=>alert("Guides")}>
+                            <a onClick={()=>onGuides()}>
                                 Guides
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a onClick={()=>alert("Towns")}>
+                            <a onClick={()=>onTowns()}>
                                 Towns
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a onClick={()=>alert("Events")}>
+                            <a onClick={()=>onEvents()}>
                                 Events
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a onClick={()=>alert("Directory")}>
+                            <a onClick={()=>onDirectory()}>
                                 Directory
                             </a>
                         </li>
                         <li className="nav-item">
-                            <a onClick={()=>alert("About")}>
+                            <a onClick={()=>onAbout()}>
                                 About
                             </a>
                         </li>
