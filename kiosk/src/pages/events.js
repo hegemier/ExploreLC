14f1024987967fc48876bc46667f1b3782afdaa7
songpeize <song_p1@denison.edu>
2020-03-04 20:53:52 -0500

    The heading of events page is finished. Adding grids

--- kiosk/src/pages/events.js
+++ kiosk/src/pages/events.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavBar } from '../components';
 import "../sass/events.scss"
+import "../sass/fonts.scss"
 
 // data-src="https://images.squarespace-cdn.com/content/v1/5bc6137be6666940992e5e76/1559500228057-LLABQMXYI35CLUH4Q7FG/ke17ZwdGBToddI8pDm48kIu3c6RI25Ef7RzjXQWq7X97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1Ua_gHjAqlkTAptD6YipitaW0yqs0Foy9-1vLCnclZojZAVle5px3XiuuAGKTYFVv9w/front-intro-bg-15.jpg"
 // data-image="https://images.squarespace-cdn.com/content/v1/5bc6137be6666940992e5e76/1559500228057-LLABQMXYI35CLUH4Q7FG/ke17ZwdGBToddI8pDm48kIu3c6RI25Ef7RzjXQWq7X97gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z5QPOohDIaIeljMHgDF5CVlOqpeNLcJ80NK65_fV7S1Ua_gHjAqlkTAptD6YipitaW0yqs0Foy9-1vLCnclZojZAVle5px3XiuuAGKTYFVv9w/front-intro-bg-15.jpg"s
@@ -9,13 +10,16 @@ const Events = ()=>{
   return (
     <div>
       <div className="event-bg">
+        <div className="overlay-div"></div>
+        <img className="overlay-bg" src="https://static1.squarespace.com/static/ta/5bc6137be6666940992e5e76/230/assets/elc-topo-top-01.png"/>
         <NavBar/>
-        <div class="text-center">
+        <div className="text-center">
           <strong className="imgHeader">See what's happening</strong>
           <p className="imgHeader-2">Events happening in Licking County</p>
           <br/>
         </div>
       </div>
+      <div className="event-date-text">Events starting from March 03, 2020</div>
     </div>
   );
 }
