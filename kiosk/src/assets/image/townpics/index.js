5f0c5faac99fa9e0947403488d138101d113cf25
Henri Hegemier <hegemi_h1@denison.edu>
2020-03-26 15:26:23 -0400

    converted towns to use a modular data model with data in a separate folder. also relocated images

---
+++ kiosk/src/assets/image/townpics/index.js
@@ -0,0 +1,13 @@
+import newark from './newark.jpg';
+import granville from './granville.jpg';
+import heath from './heath.jpg';
+import utica from './utica.jpg';
+import johnstown from './johnstown.jpg';
+import hebron from './hebron.jpg';
+import buckeyelake from './buckeyelake.jpg';
+import hartford from './hartford.jpg';
+import pataskala from './pataskala.jpg';
+
+export {
+    newark, granville, heath, utica, johnstown, hebron, buckeyelake, hartford, pataskala
+};
